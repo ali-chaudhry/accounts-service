b5ff0ae7f0986f158b61c0f93d60560f2b5b8a7a
Ali Anwar <ali.anwar33@gmail.com>
2022-01-19 17:32:58 +0500

    service added

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const initApp = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Accounts Sercvice')
     .setDescription('REST APIs description')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, document);
-  await app.listen(5002);
+  const document = SwaggerModule.createDocument(initApp, config);
+  SwaggerModule.setup('api/swagger', initApp, document);
+  await initApp.listen(5002);
+
+  const initService = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { host: '0.0.0.0', port: 5003 },
+    },
+  );
+
+  await initService.listen();
 }
 bootstrap();
