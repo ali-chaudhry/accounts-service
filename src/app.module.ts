767b192a048862d5242ca3c7528accf401e2352c
Ali Anwar <ali.anwar33@gmail.com>
2022-01-19 13:53:22 +0500

    added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupplierModule } from './supplier/supplier.module';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [SupplierModule, UserModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
