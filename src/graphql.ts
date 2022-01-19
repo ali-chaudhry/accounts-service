7f78e2ec17592ca1af86bd3feb4daa0e324d6635
Ali Anwar <ali.anwar33@gmail.com>
2022-01-19 17:33:57 +0500

    updated

--- src/graphql.ts
+++ src/graphql.ts
@@ -7,30 +7,27 @@
 
 /* tslint:disable */
 /* eslint-disable */
-export class CreateUserInput {
+export interface CreateUserInput {
     exampleField?: Nullable<number>;
 }
 
-export class UpdateUserInput {
+export interface UpdateUserInput {
     id: number;
 }
 
-export class User {
+export interface User {
     exampleField?: Nullable<number>;
 }
 
-export abstract class IQuery {
-    abstract users(): Nullable<User>[] | Promise<Nullable<User>[]>;
-
-    abstract user(id: number): Nullable<User> | Promise<Nullable<User>>;
+export interface IQuery {
+    users(): Nullable<User>[] | Promise<Nullable<User>[]>;
+    user(id: number): Nullable<User> | Promise<Nullable<User>>;
 }
 
-export abstract class IMutation {
-    abstract createUser(createUserInput: CreateUserInput): User | Promise<User>;
-
-    abstract updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
-
-    abstract removeUser(id: number): Nullable<User> | Promise<Nullable<User>>;
+export interface IMutation {
+    createUser(createUserInput: CreateUserInput): User | Promise<User>;
+    updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+    removeUser(id: number): Nullable<User> | Promise<Nullable<User>>;
 }
 
 type Nullable<T> = T | null;
