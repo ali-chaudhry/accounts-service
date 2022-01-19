ba9db0b7c14864df312473b72fa15d745e3e343f
Ali Anwar <ali.anwar33@gmail.com>
2022-01-19 15:25:29 +0500

    swagger and health endpoints added

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Accounts Sercvice')
+    .setDescription('REST APIs description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/swagger', app, document);
   await app.listen(5002);
 }
 bootstrap();
