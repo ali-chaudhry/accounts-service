1acdfe946178dc26d5caae1c97029391abc078c9
FaisalAmeer225 <90785273+FaisalAmeer225@users.noreply.github.com>
2022-01-27 16:23:24 +0500

    Commit: Fix Conflicts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,11 @@
 
-import { Module,forwardRef  } from '@nestjs/common';
 
 import { Module,forwardRef } from '@nestjs/common';
-
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserResolver } from './user.resolver';
 import { User } from './entities/user.entity';
 import { jwtAuthFactory } from 'src/utils/jwt.helper';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
 import { PasswordHasherService } from 'src/auth/password-hasher/password-hasher.service';
 import { JwtModule} from 'src/auth';
@@ -22,13 +19,6 @@ import { JWT_MODULE_OPTIONS } from 'src/auth/jwt.constants';
     signOptions: { expiresIn: '60s' },
   })
 ],
-
-
-  JwtModule.registerAsync({
-    useFactory: jwtAuthFactory
-  }),
-],
-
   providers: [UserResolver, UserService, PasswordHasherService],
 })
 export class UserModule {}
