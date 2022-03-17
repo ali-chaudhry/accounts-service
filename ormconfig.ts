18bc639d65b3b0666b3c7cfc1d870be8bb325af6
Ali Anwar <ali.anwar33@gmail.com>
2022-03-17 14:30:10 +0500

    postgres connection updated

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,16 +4,13 @@ const envFile = '/tmp/env';
 
 process.env = { ...process.env, ...config.config({ path: envFile }).parsed };
 
-console.log(process.env.NODE_ENV, process.env.DB_HOST);
 export = {
-  // ...configutations().database,
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5432,
+  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  //entities: [join(__dirname, '**', '*.entity.{ts,js}')],
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
   migrationsTableName: 'migrations_typeorm',
